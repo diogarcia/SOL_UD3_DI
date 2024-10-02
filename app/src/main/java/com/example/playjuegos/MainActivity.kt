9abdd17b13f2a7ba0eda45b3b2e05d8b63b4b23c
dioga <diogarcia2@gmail.com>
2024-10-02 21:31:31 +0200

    Solución Ej1 DI DAM_SEMI 24/25 by Dionisio García

---
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.playjuegos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val jugadorBtn = findViewById(R.id.button2) as Button
+        jugadorBtn.setOnClickListener{ lanzarNuevoJugador() }
+
+    }
+    fun lanzarNuevoJugador(){
+        val i = Intent(this, NewPlayer::class.java)
+        startActivity(i)
+    }
+}
